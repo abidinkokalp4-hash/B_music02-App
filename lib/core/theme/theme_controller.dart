af613e835b7eb92bd122c799fd6508e079864ad2
abidinkokalp4-hash <abidinkokalp4@gmail.com>
2026-09-12 10:09:00 +0300

    Update theme_controller.dart

--- lib/core/theme/theme_controller.dart
+++ lib/core/theme/theme_controller.dart
@@ -2,39 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeController extends ChangeNotifier {
-  static const String _storageKey = 'app_theme_mode';
+  static const String _storageKey =
+      'app_theme_mode';
 
-  ThemeMode _themeMode = ThemeMode.system;
+  ThemeMode _themeMode =
+      ThemeMode.system;
 
-  ThemeMode get themeMode => _themeMode;
+  ThemeMode get themeMode =>
+      _themeMode;
 
   bool get isSystem =>
-      _themeMode == ThemeMode.system;
+      _themeMode ==
+      ThemeMode.system;
 
   bool get isDark =>
-      _themeMode == ThemeMode.dark;
+      _themeMode ==
+      ThemeMode.dark;
 
   bool get isLight =>
-      _themeMode == ThemeMode.light;
+      _themeMode ==
+      ThemeMode.light;
 
   Future<void> load() async {
     final prefs =
-        await SharedPreferences.getInstance();
+        await SharedPreferences
+            .getInstance();
 
     final savedMode =
-        prefs.getString(_storageKey);
+        prefs.getString(
+      _storageKey,
+    );
 
     switch (savedMode) {
       case 'dark':
-        _themeMode = ThemeMode.dark;
+        _themeMode =
+            ThemeMode.dark;
         break;
 
       case 'light':
-        _themeMode = ThemeMode.light;
+        _themeMode =
+            ThemeMode.light;
         break;
 
       default:
-        _themeMode = ThemeMode.system;
+        _themeMode =
+            ThemeMode.system;
     }
 
     notifyListeners();
@@ -88,11 +100,41 @@ class ThemeController extends ChangeNotifier {
     notifyListeners();
 
     final prefs =
-        await SharedPreferences.getInstance();
+        await SharedPreferences
+            .getInstance();
 
     await prefs.setString(
       _storageKey,
       value,
     );
+  }
+}
+
+class ThemeControllerScope
+    extends InheritedNotifier<
+        ThemeController> {
+  const ThemeControllerScope({
+    super.key,
+    required ThemeController
+        controller,
+    required super.child,
+  }) : super(
+          notifier: controller,
+        );
+
+  static ThemeController of(
+    BuildContext context,
+  ) {
+    final scope =
+        context
+            .dependOnInheritedWidgetOfExactType<
+                ThemeControllerScope>();
+
+    assert(
+      scope != null,
+      'ThemeControllerScope bulunamadı.',
+    );
+
+    return scope!.notifier!;
   }
 }
