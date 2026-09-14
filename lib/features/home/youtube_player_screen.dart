8456b6a88d71880d3e0590dce7e3ed1c76dec398
abidinkokalp4-hash <abidinkokalp4@gmail.com>
2026-09-14 21:57:09 +0300

    fix: use YouTube compliant HTTPS WebView referrer

--- lib/features/home/youtube_player_screen.dart
+++ lib/features/home/youtube_player_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -29,8 +27,10 @@ class _YouTubePlayerScreenState extends State<YouTubePlayerScreen> {
   late final WebViewController _web;
   bool _loading = true;
 
-  static const String _androidAppReferrer =
-      'android-app://com.example.b_music02';
+  // YouTube requires mobile WebView embeds to identify the app with an
+  // HTTPS Referer. loadHtmlString(baseUrl: ...) makes the iframe request use
+  // this app identity as its Referer.
+  static const String _appOrigin = 'https://com.example.b_music02';
 
   @override
   void initState() {
@@ -60,26 +60,43 @@ class _YouTubePlayerScreenState extends State<YouTubePlayerScreen> {
       'playsinline': '1',
       'rel': '0',
       'enablejsapi': '1',
-      'origin': 'https://www.youtube.com',
+      'origin': _appOrigin,
+      'widget_referrer': _appOrigin,
     };
     if (start > 0) params['start'] = '$start';
     if (end != null && end > start) params['end'] = '$end';
 
-    final uri = Uri.https(
+    final embedUrl = Uri.https(
       'www.youtube.com',
       '/embed/${widget.item.videoId}',
       params,
-    );
+    ).toString();
 
-    final headers = <String, String>{
-      'Accept-Language': 'tr-TR,tr;q=0.9,en;q=0.8',
-    };
+    final html = '''
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <meta name="referrer" content="strict-origin-when-cross-origin">
+  <style>
+    html,body{margin:0;padding:0;width:100%;height:100%;background:#070812;overflow:hidden}
+    iframe{display:block;border:0;width:100%;height:100%;background:#070812}
+  </style>
+</head>
+<body>
+  <iframe
+    src="$embedUrl"
+    title="YouTube video player"
+    allow="autoplay; encrypted-media; picture-in-picture; fullscreen"
+    referrerpolicy="strict-origin-when-cross-origin"
+    allowfullscreen>
+  </iframe>
+</body>
+</html>
+''';
 
-    if (Platform.isAndroid) {
-      headers['Referer'] = _androidAppReferrer;
-    }
-
-    await _web.loadRequest(uri, headers: headers);
+    await _web.loadHtmlString(html, baseUrl: _appOrigin);
   }
 
   Future<void> _openExternal() async {
